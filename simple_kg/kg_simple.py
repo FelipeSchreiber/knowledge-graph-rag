e72dcd1b8d6ba9da3142678a0a7234f4c7603de7
Felipe Schreiber Fernandes <schreiber.felipe@gmail.com>
2024-10-01 10:36:49 -0300

    Update kg_simple.py

--- simple_kg/kg_simple.py
+++ simple_kg/kg_simple.py
@@ -1,43 +1,36 @@
 from dotenv import load_dotenv
 import os
 from neo4j import GraphDatabase
-
 load_dotenv()
 
-
 AURA_INSTANCENAME = os.environ["AURA_INSTANCENAME"]
+NEO4J_DATABASE=os.environ["NEO4J_DATABASE"]
 NEO4J_URI = os.environ["NEO4J_URI"]
-NEO4J_USERNAME = os.environ["NEO4J_USERNAME"]
-NEO4J_PASSWORD = os.environ["NEO4J_PASSWORD"]
-NEO4J_DATABASE = os.environ["NEO4J_DATABASE"]
-AUTH = (NEO4J_USERNAME, NEO4J_PASSWORD)
+NEO4J_USERNAME=os.environ["NEO4J_USERNAME"]
+NEO4J_PASSWORD=os.environ["NEO4J_PASSWORD"]
+AURA_INSTANCEID=os.environ["AURA_INSTANCEID"]
 
-driver = GraphDatabase.driver(NEO4J_URI, auth=AUTH, database=NEO4J_DATABASE)
-
-
-def connect_and_query():
-    # driver = GraphDatabase.driver(NEO4J_URI, auth=AUTH)
-    try:
-        with driver.session(database=NEO4J_DATABASE) as session:
-            result = session.run("MATCH (n) RETURN count(n)")
-            count = result.single().value()
-            print(f"Number of nodes: {count}")
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        driver.close()
-
-
+AUTH = (NEO4J_USERNAME,NEO4J_PASSWORD)
+driver = GraphDatabase.driver(NEO4J_URI,auth=AUTH)
+try:
+    driver.verify_connectivity()
+    print("Connection established.")
+    with driver.session(database=NEO4J_DATABASE) as session:
+        result = session.run("MATCH (n) RETURN count (n)")
+        count = result.single().value()
+        print(f"Number of nodes is {count}")
+except:
+    print("Connection not established.")
+# finally:
+#     driver.close()
+    
 def create_entities(tx):
-    # Create Albert Einstein node
-    tx.run("MERGE (a:Person {name: 'Albert Einstein'})")
-
-    # Create other nodes
+    tx.run("MERGE (a:Person {name:'Albert Einstein'})")
     tx.run("MERGE (p:Subject {name: 'Physics'})")
     tx.run("MERGE (n:NobelPrize {name: 'Nobel Prize in Physics'})")
     tx.run("MERGE (g:Country {name: 'Germany'})")
     tx.run("MERGE (u:Country {name: 'USA'})")
-
+    
 
 def create_relationships(tx):
     # Create studied relationship
@@ -83,8 +76,8 @@ def query_graph_simple(cypher_query):
                 print(record["name"])
     except Exception as e:
         print(f"Error: {e}")
-    finally:
-        driver.close()
+    # finally:
+    #     driver.close()
 
 
 # Function to connect and run a Cypher query
@@ -97,8 +90,8 @@ def query_graph(cypher_query):
                 print(record["path"])
     except Exception as e:
         print(f"Error: {e}")
-    finally:
-        driver.close()
+    # finally:
+    #     driver.close()
 
 
 def build_knowledge_graph():
@@ -113,8 +106,8 @@ def build_knowledge_graph():
 
     except Exception as e:
         print(f"Error: {e}")
-    finally:
-        driver.close()
+    # finally:
+    #     driver.close()
 
 
 # Cypher query to find paths related to Albert Einstein
@@ -132,6 +125,7 @@ MATCH path=(a:Person {name: 'Albert Einstein'})-[:DIED_IN]->(u:Country)
 RETURN path
 """
 
+
 # Simple Cypher query to find all node names
 simple_query = """
 MATCH (n)
@@ -139,13 +133,15 @@ RETURN n.name AS name
 """
 
 if __name__ == "__main__":
+    # with driver.session() as session:
+    #     session.run(f"DROP DATABASE {NEO4J_DATABASE} IF EXISTS")
     # Build the knowledge graph
     # build_knowledge_graph()
 
-    # query_graph_simple(
-    #     simple_query
-    # )
+    query_graph_simple(simple_query)
+    
     query_graph(einstein_query)
+    driver.close()
 
 
 # Run this to see the entire graph in the neo4j browser/console
